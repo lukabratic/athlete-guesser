767c06a40520e65dab926b5ca9cacd42bec24b9f
Git Immersion - Homework <luka.bratic0824@gmail.com>
2022-03-29 21:22:05 -0400

    Changes

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ const app = express();
 
 //configure app
 let port = process.env.PORT || 3000;
-let host = 'localhost';
 app.set('view engine', 'ejs');
 
-app.listen(port, host, () => {
+app.listen(port, () => {
     console.log("Server is running on port", port);
 });
 
